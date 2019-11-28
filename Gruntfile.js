bc7c73ea79b3d28c16ca5aade97877dc4d9df6a2
home <andybaaall>
2019-11-28 21:09:10 +1300

    set up gruntfile (whew!) and style.css

---
+++ Gruntfile.js
@@ -0,0 +1,147 @@
+// this is still a template!!
+
+module.exports = function(grunt) {
+
+    // Project configuration
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        // meta
+        meta: {
+            src: 'dev',
+            dest: 'dist',
+            modules: 'node_modules',
+        },
+        // Task options
+        copy: {
+            main: {
+                files: [
+                    {
+                        // php
+                        expand: true,
+                        cwd: 'dev',
+                        src: '**/*.php',
+                        flatten: true,
+                        filter: 'isFile',
+                        dest: 'dist/',
+                    },
+                    {
+                        // .min.css
+                        expand: true,
+                        cwd: '<%= meta.src %>',
+                        src: '**/*.min.css',
+                        flatten: true,
+                        filter: 'isFile',
+                        dest: '<%= meta.dest %>/css'
+                    },
+                    {
+                        // .min.js
+                        expand: true,
+                        cwd: '<%= meta.src %>',
+                        src: '**/*.min.js',
+                        flatten: true,
+                        filter: 'isFile',
+                        dest: '<%= meta.dest %>/js'
+                    },
+                    {
+                        // bootstrap - JS
+                        expand: true,
+                        cwd: '<%= meta.modules %>',
+                        src: 'bootstrap/dist/js/bootstrap.min.js',
+                        flatten: true,
+                        filter: 'isFile',
+                        dest: '<%= meta.dest %>/js'
+                    },
+                    {
+                        // bootstrap - CSS
+                        expand: true,
+                        cwd: '<%= meta.modules %>',
+                        src: 'bootstrap/dist/css/bootstrap.min.css',
+                        flatten: true,
+                        filter: 'isFile',
+                        dest: '<%= meta.dest %>/css'
+                    },
+                    {
+                        // popper.js
+                        expand: true,
+                        cwd: '<%= meta.modules %>',
+                        src: 'popper.js/dist/umd/popper.min.js',
+                        flatten: true,
+                        filter: 'isFile',
+                        dest: '<%= meta.dest %>/js'
+                    },
+                    {
+                        // image folder
+                        expand: true,
+                        cwd: '<%= meta.src %>',
+                        src: 'assets/img/**',
+                        flatten: true,
+                        filter: 'isFile',
+                        dest: '<%= meta.dest %>/img'
+                    }
+                ],
+            }
+        },
+        csslint: {
+            strict: {
+                options: {
+                    import: 2
+                },
+                src: ['dev/assets/css/master.css']
+            },
+        },
+        cssmin: {
+            target: {
+                files: [{
+                    expand: true,
+                    src: ['dev/assets/css/master.css'],
+                    dest: '',
+                    ext: '.min.css'
+                }]
+            }
+        },
+        jshint: {
+            files: ['dev/assets/js/script.js'],
+            options: {
+                unused: true,
+                esversion: 6
+            }
+        },
+        shell: {
+            options: {
+                stderr: false
+            },
+            target: {
+                command: 'cp -r /Users/test/Documents/workingFiles/wild-about-weeds/dist /Applications/MAMP/htdocs/wild-about-weeds/wp-content/themes && mv /Applications/MAMP/htdocs/wild-about-weeds/wp-content/themes/dist /Applications/MAMP/htdocs/wild-about-weeds/wp-content/themes/wild-about-weeds'
+            },
+        },
+        uglify: {
+            my_target: {
+                files: {
+                    'dev/assets/js/script.min.js': ['dev/assets/js/script.js']
+                }
+            }
+        },
+        watch: {
+            files: ['dev/assets/js/script.js', 'dev/assets/css/master.css'],
+            tasks: ['jshint', 'csslint', 'uglify', 'cssmin']
+        }
+    });
+
+    // Loading tasks
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-csslint');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-uglify-es');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-shell');
+
+
+    // Registering tasks
+    grunt.registerTask('default', function() {
+        grunt.log.write('Grunt is up and running!').ok();
+    });
+    
+    grunt.registerTask('makeDist', ['copy', 'shell']);
+    grunt.registerTask('runWatch', ['watch']);
+};
