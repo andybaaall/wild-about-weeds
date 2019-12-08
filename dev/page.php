556f36455657c5b352bfa214422f51a9c83c3558
home <andybaaall>
2019-12-08 20:49:37 +1300

    added page.php

---
+++ dev/page.php
@@ -0,0 +1,14 @@
+<?php get_header(); ?>
+
+<?php if (have_posts() ): ?>
+    <?php while ( have_posts(get_the_ID()) ): ?>
+        <?php the_post(); ?>
+        <div class="container page-content overflow-scroll">
+            <h1><?php echo the_title(); ?></h1>
+            <small><?php echo the_date(); ?></small>
+            <?php echo the_content(); ?>
+        </div>
+    <?php endwhile; ?>
+<?php endif; ?>
+
+<?php get_footer(); ?>
