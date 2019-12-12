8202da3dcc1ad1c548b2d24472dabc6e52414441
home <andybaaall>
2019-12-13 09:06:27 +1300

    master branch prepared for install

--- dist/inc/services_fields.php
+++
@@ -1,4 +0,0 @@
-<div class="">
-    <label for="">Pricing</label>
-    <input type="textarea" name="wildWeeds_servicePricing" value="<?php echo get_post_meta(get_the_ID(), 'wildWeeds_servicePricing', true); ?>" placeholder="Enter something like '$75 per hour', or 'Get in touch for a free quote'." style="width: 100%;">
-</div>
